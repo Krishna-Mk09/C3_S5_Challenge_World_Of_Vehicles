e40c85ee9e13158fb6904d75b3a5382b202744d4
M V K <krishna.mugala9@gmail.com>
2022-08-30 22:13:49 +0530

    created the mp5 class in the pckge

--- src/main/java/com/niit/jdp/ModelMP4.java
+++ src/main/java/com/niit/jdp/ModelMP4.java
@@ -12,22 +12,41 @@ public class ModelMP4 extends Transmission implements TypeOfTransmission {
 
     @Override
     public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4) {
+        System.out.println("gearRatio1 = " + gearRatio1);
+        System.out.println("gearRatio2 = " + gearRatio2);
+        System.out.println("gearRatio3 = " + gearRatio3);
+        System.out.println("gearRatio4 = " + gearRatio4);
+
 
     }
 
     @Override
     public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5) {
-
+        System.out.println("gearRatio1 = " + gearRatio1);
+        System.out.println("gearRatio2 = " + gearRatio2);
+        System.out.println("gearRatio3 = " + gearRatio3);
+        System.out.println("gearRatio4 = " + gearRatio4);
+        System.out.println("gearRatio4 = " + gearRatio5);
     }
 
     @Override
     public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6) {
-
+        System.out.println("gearRatio1 = " + gearRatio1);
+        System.out.println("gearRatio2 = " + gearRatio2);
+        System.out.println("gearRatio3 = " + gearRatio3);
+        System.out.println("gearRatio4 = " + gearRatio4);
     }
 
     @Override
     public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6, float gearRatio7, float gearRatio8) {
-
+        System.out.println("gearRatio1 = " + gearRatio1);
+        System.out.println("gearRatio2 = " + gearRatio2);
+        System.out.println("gearRatio3 = " + gearRatio3);
+        System.out.println("gearRatio4 = " + gearRatio4);
+        System.out.println("gearRatio1 = " + gearRatio1);
+        System.out.println("gearRatio2 = " + gearRatio2);
+        System.out.println("gearRatio3 = " + gearRatio3);
+        System.out.println("gearRatio4 = " + gearRatio4);
     }
 
     public ModelMP4() {
--- src/main/java/com/niit/jdp/TransmissionImpl.java
+++ src/main/java/com/niit/jdp/TransmissionImpl.java
@@ -10,6 +10,10 @@ package com.niit.jdp;
 
 public class TransmissionImpl {
     public static void main(String[] args) {
+        ModelMP4 model1 = new ModelMP4("manual", "mp4", 4);
+        model1.gearRatio(2.540f, 1.920f, 1.510f, 1.000f);
+        System.out.println("Type Of Transmission Manual and Model Details = " + model1);
+
 
     }
 }
