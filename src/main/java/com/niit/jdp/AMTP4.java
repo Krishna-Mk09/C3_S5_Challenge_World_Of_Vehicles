500f07a30e97112d05572167c3004d2cd4fdc1b3
M V K <krishna.mugala9@gmail.com>
2022-08-31 00:17:21 +0530

    added the methods in the amtp4 class

--- src/main/java/com/niit/jdp/AMTP4.java
+++ src/main/java/com/niit/jdp/AMTP4.java
@@ -13,4 +13,9 @@ public class AMTP4 extends TypeOfTransmissions {
     public AMTP4(String transmissionType, String modelNumber, int noOfForwardGears) {
         super(transmissionType, modelNumber, noOfForwardGears);
     }
+
+    @Override
+    public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4) {
+        super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4);
+    }
 }
