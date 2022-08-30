28235571e06e220b5d91dd64ffb40a631763aca5
M V K <krishna.mugala9@gmail.com>
2022-08-30 23:46:11 +0530

    added methods in md6 class

--- src/main/java/com/niit/jdp/ModelMD6.java
+++ src/main/java/com/niit/jdp/ModelMD6.java
@@ -14,5 +14,11 @@ public class ModelMD6 extends TypeOfTransmissions {
 
     public ModelMD6(String transmissionType, String modelNumber, int noOfForwardGears) {
         super(transmissionType, modelNumber, noOfForwardGears);
+
+    }
+
+    @Override
+    public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6) {
+        super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6);
     }
 }
