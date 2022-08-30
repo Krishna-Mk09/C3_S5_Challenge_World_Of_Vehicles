a831d9ef609e3b84b5b6ac02ec8d56aa11c0359b
M V K <krishna.mugala9@gmail.com>
2022-08-30 22:34:34 +0530

    Added the method in mp6 class

--- src/main/java/com/niit/jdp/ModelMP6.java
+++ src/main/java/com/niit/jdp/ModelMP6.java
@@ -8,5 +8,9 @@
 
 package com.niit.jdp;
 
-public class ModelMP6 {
+public class ModelMP6 extends TypeOfTransmissions {
+    @Override
+    public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6) {
+        super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6);
+    }
 }
