c330dee9d2bd64dcdbb3253cd319fcf946520ae0
M V K <krishna.mugala9@gmail.com>
2022-08-30 22:40:01 +0530

    added no args and added parameterized constructor

--- src/main/java/com/niit/jdp/ModelMP8.java
+++ src/main/java/com/niit/jdp/ModelMP8.java
@@ -13,4 +13,11 @@ public class ModelMP8 extends TypeOfTransmissions {
     public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6, float gearRatio7, float gearRatio8) {
         super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6, gearRatio7, gearRatio8);
     }
+
+    public ModelMP8() {
+    }
+
+    public ModelMP8(String transmissionType, String modelNumber, int noOfForwardGears) {
+        super(transmissionType, modelNumber, noOfForwardGears);
+    }
 }
