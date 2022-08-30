431730a3433b4491c195614872e9a4f69adfb62c
M V K <krishna.mugala9@gmail.com>
2022-08-31 00:24:41 +0530

    added the method in the ampt5 class

--- src/main/java/com/niit/jdp/AMTD5.java
+++ src/main/java/com/niit/jdp/AMTD5.java
@@ -9,6 +9,11 @@
 package com.niit.jdp;
 
 public class AMTD5 extends TypeOfTransmissions {
+    @Override
+    public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5) {
+        super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5);
+    }
+
     public AMTD5() {
     }
 
