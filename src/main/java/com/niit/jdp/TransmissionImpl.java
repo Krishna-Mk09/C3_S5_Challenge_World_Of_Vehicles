a503524cc6df3920ad75ac966ceab2d235a771fa
M V K <krishna.mugala9@gmail.com>
2022-08-30 22:55:17 +0530

    called the methods and initialised the objects

--- src/main/java/com/niit/jdp/TransmissionImpl.java
+++ src/main/java/com/niit/jdp/TransmissionImpl.java
@@ -12,11 +12,16 @@ public class TransmissionImpl {
     public static void main(String[] args) {
         TypeOfTransmissions model1 = new TypeOfTransmissions("manual", "mp4", 4);
         ModelMP5 model2 = new ModelMP5("MANUAL", "MP5", 5);
+        ModelMP6 model3 = new ModelMP6("MANUAL", "MP6", 6);
+        ModelMP8 model4 = new ModelMP8("automatic", "automaticCVT", 8);
+        ModelMP8 model5 = new ModelMP8();
         System.out.println("Type Of Transmission Manual and Model Details = " + model1);
         model1.gearRatio(2.540f, 1.920f, 1.510f, 1.000f);
         System.out.println("model2 = " + model2);
         model2.gearRatio(1.22f, 5.22f, 5.22f, 5.22f, 6.224f);
-
-
+        System.out.println("model3 = " + model3);
+        model3.gearRatio(3.010f, 2.070f, 1.43f, 1.000f, 0.710f, 0.570f);
+        System.out.println("model3 = " + model4);
+        model4.gearRatio(4.714f, 3.143f, 2.106f, 1.667f, 1.356f, 1.000f, 5.678f, 6.789f);
     }
 }
