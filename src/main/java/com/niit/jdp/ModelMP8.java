9516b91b285df8050f1264208960b7cb60a4ac04
M V K <krishna.mugala9@gmail.com>
2022-08-30 22:39:07 +0530

    added methods mp8 class

--- src/main/java/com/niit/jdp/ModelMP8.java
+++ src/main/java/com/niit/jdp/ModelMP8.java
@@ -8,5 +8,9 @@
 
 package com.niit.jdp;
 
-public class ModelMP8 {
+public class ModelMP8 extends TypeOfTransmissions {
+    @Override
+    public void gearRatio(float gearRatio1, float gearRatio2, float gearRatio3, float gearRatio4, float gearRatio5, float gearRatio6, float gearRatio7, float gearRatio8) {
+        super.gearRatio(gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6, gearRatio7, gearRatio8);
+    }
 }
